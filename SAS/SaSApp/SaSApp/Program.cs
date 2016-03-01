464b1235a6b8b0b38bc5abd3de79075e00801ecb
Robert Bermejo <pollobull12@hotmail.com>
2016-03-01 18:08:57 +0100

    Crate a SAS example

---
+++ SAS/SaSApp/SaSApp/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace SaSApp
+{
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Storage connectionstring
+            var storageAccount = CloudStorageAccount.Parse("Your Blob Storage Connection String");
+            var cloudBlobClient = storageAccount.CreateCloudBlobClient();
+            //GetContainer on insert blob. Create reference if no exist
+            var blobContainer = cloudBlobClient.GetContainerReference("ContainerName");
+            blobContainer.CreateIfNotExists();
+            //Create referennce to file
+            var blockBlob =  blobContainer.GetBlockBlobReference("filename.extension");
+            //Create Permisions.
+            var sasToken = blockBlob.Container.GetSharedAccessSignature(new SharedAccessBlobPolicy()
+            {
+                Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write,
+                SharedAccessStartTime = DateTime.UtcNow.AddMinutes(-15),
+                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(15)                
+            });
+
+            Console.WriteLine($"token SAS: {sasToken}");
+            Console.ReadLine();
+        }
+    }
+}
